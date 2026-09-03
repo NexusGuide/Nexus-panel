72fc88a1041e6da26c0fff2c24aa40872bb7ea0b
NexusGuide <NexusGuidem1@gmail.com>
2026-09-03 19:58:11 +0330

    chore: release 0.0.2

--- app/version.py
+++ app/version.py
@@ -1,5 +1,5 @@
 # This fork versions itself independently of the PasarGuard release it is built
 # on. UPSTREAM_VERSION records that release, so anything that needs to know
 # which upstream code is running still has the real number.
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 UPSTREAM_VERSION = "5.3.0"
